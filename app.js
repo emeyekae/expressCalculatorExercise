35c77e503af8a6a642796955db4fb074690233cb
Vincent Ngondi <30436045+gizatupu@users.noreply.github.com>
2023-12-23 21:30:55 +0300

    Update app.js

--- app.js
+++ app.js
@@ -79,3 +79,13 @@ app.get('/mean', function(req, res, next) {
   app.listen(3000, function() {
     console.log(`Server starting on port 3000`);
   });
+
+
+//Suggestions
+//1. There is some code duplication in the route handlers (/mean, /median, /mode). You can create a common function to handle the common logic of parsing the nums query parameter and throwing errors if necessary.
+//2. The status property in the error handler could be more consistently formatted. 
+//status: `${err.status} Bad Request`
+//3.The error handler uses a status code of 500 for all errors. It might be beneficial to return more appropriate HTTP status codes based on the type of error (e.g., 400 for client errors, 404 for not found, etc.).
+//4.It's a good practice to implement a graceful shutdown mechanism for your server. For example, you can listen for the SIGINT signal to gracefully close the server when the process is terminated.
+//5.Ensure that you're validating and sanitizing user input to prevent potential security vulnerabilities like SQL injection or other types of attacks. In this case, validating and parsing the input for the nums parameter is critical
+//6.Consider using environment variables for configuration, such as specifying the port number. This makes your application more configurable and adaptable to different environments.
